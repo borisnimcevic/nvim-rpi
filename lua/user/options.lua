9f791c260af57639f988f43c03541946880fb455
Boris Nimcevic <borisnimcevic@gmail.com>
2022-01-09 21:54:27 +0100

    Moved options to lua.options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,2 +1,35 @@
 vim.opt.backup = false
-vim.opt.clipboard = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 1
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.conceallevel = 0
+vim.opt.fileencoding = "utf-8"
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+vim.opt.pumheight = 10
+vim.opt.showmode = false
+vim.opt.showtabline = 2 -- 0 for never, 1 for only when there is more than 1, 2 always
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.timeoutlen = 1000 --ms
+vim.opt.undofile = true
+vim.opt.updatetime = 300
+vim.opt.writebackup = false
+vim.opt.shiftwidth = 4 -- number of spaces inserted for each indetation
+vim.opt.tabstop = 4
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+-- vim.opt.wrap = true
+-- vim.opt.sidescrolloff = 8
+vim.opt.guifont = "monospace:h17"
+vim.opt.shortmess:append "c"
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
