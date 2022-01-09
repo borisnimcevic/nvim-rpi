e88f8cf7cc39bc3ce193fc365076ede7d2688c62
Boris Nimcevic <borisnimcevic@gmail.com>
2022-01-09 20:11:50 +0100

    step one lunarvim from scratch

---
+++ init.lua
@@ -0,0 +1,4 @@
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 2
+vim.opt.completeopt = { "menuone", "noselect" }
---
+++ lua/user/options.lua
@@ -0,0 +1,2 @@
+vim.opt.backup = false
+vim.opt.clipboard = false
